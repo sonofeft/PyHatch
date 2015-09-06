489af5c4f1e5c01b48f78ea479ebb279fd9cdfaf
sonofeft <ctatsourceforge@gmail.com>
2015-09-05 22:39:40 -0700

    Version 0.0.14

--- pyhatch/_version.py
+++ pyhatch/_version.py
@@ -10,4 +10,4 @@ Used to set version info throughout the project.
 
 ------
 """
-__version__ = '0.0.13'  # METADATA_RESET:__version__ = '<<version>>'
+__version__ = '0.0.14'  # METADATA_RESET:__version__ = '<<version>>'
